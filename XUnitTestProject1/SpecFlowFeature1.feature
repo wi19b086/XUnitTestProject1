5739c5cc109a958128af074b2f75851bbc045d87
Hasan Aras <wi19b086@technikum-wien.at>
2020-11-04 21:47:00 +0100

    Projektdateien hinzufügen.

---
+++ XUnitTestProject1/SpecFlowFeature1.feature
@@ -0,0 +1,21 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Sinus
+    Given the first number is PI
+    When sinus is taken
+    Then the result should be 0
+
+Scenario: Cosinus
+    Given the first number is PI
+    When cosinus is taken
+    Then the result should be -1
+
+Scenario: tangens
+    Given the first number is PI
+    When tangens is taken
+    Then the result should be 0
+
---
+++ XUnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number, x;
+        [Given(@"the first number is PI")]
+        public void GivenTheFirstNumberIsPI()
+        {
+            number = Math.PI;
+        }
+        
+        [When(@"sinus is taken")]
+        public void WhenSinusIsTaken()
+        {
+            x = Math.Sin(number);
+        }
+        
+        [When(@"cosinus is taken")]
+        public void WhenCosinusIsTaken()
+        {
+            x = Math.Cos(number);
+        }
+        
+        [When(@"tangens is taken")]
+        public void WhenTangensIsTaken()
+        {
+            x = Math.Tan(number);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, x, 5);
+        }
+    }
+}
